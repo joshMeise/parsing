bb891c652d5bcf2aa8c25bc7d5856928baccaa22
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2024-06-18 12:36:03 -0400

    more http tests added:

--- http/tests/tv.sh
+++ http/tests/tv.sh
@@ -13,6 +13,10 @@ echo -n "OPTIONS /anypage.html HTTP/1.0" > ./pass.8
 echo -n "GET https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages HTTP/1.1" > ./pass.9
 echo -n "CONNECT developer.mozilla.org:80 HTTP/1.1" > ./pass.10
 echo -n "HTTP/1.1 404 Not Found" > ./pass.11
+echo -n "HTTP/1.1 100 Continue" > ./pass.12
+echo -n "HTTP/1.1 201 Created" > ./pass.13
+echo -n "HTTP/1.1 302 Found" > ./pass.14
+echo -n "HTTP/1.1 503 Service Unavailable" > ./pass.15
 
 # [FAILING TESTS]
 echo -n "HTTP/11.1" > ./fail.1
@@ -22,6 +26,12 @@ echo -n "HTTP/.1" > ./fail.4
 echo -n "PUT - HTTP/1.0" > ./fail.5
 echo -n "GET fojan HTTP/1.1" > ./fail.6
 echo -n "HTTP/1.1 704 Not Found" > ./fail.7
+echo -n "GST /where?q=now HTTP/1.1" > ./fail.8
+echo -n "OPTIONS /anypage.html HTTP/19.0" > ./fail.9
+echo -n "OPTIONS words HTTP/1.0" > ./fail.10
+echo -n "HTTP/1.1 52 Bad" > ./fail.11
+echo -n "HTTP/1.1 1 Good" > ./fail.12
+echo -n "HTTP/1.1 2345 Bad" > ./fail.13
 
 # generate a file with the number of pass and fail tests
 #NUMPASS=(pass.*)
